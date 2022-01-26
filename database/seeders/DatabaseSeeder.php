d8a491cda0a5f6ad3c3ab25b1ef071e685703948
joyanta1000 <joyanta955@gmail.com>
2022-01-26 15:55:19 +0600

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,20 +17,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Country::factory(2)->create();
+        \App\Models\Country::factory(100)->create();
         \App\Models\City::factory()
-            ->has(\App\Models\Country::factory()->count(2), 'country')
+            ->has(\App\Models\Country::factory()->count(1), 'country')
             ->state(new Sequence(
                 fn ($sequence) => ['countryid' => Country::all()->random()],
             ))
-            ->count(4)
+            ->count(100*100)
             ->create();
         \App\Models\Area::factory()
-            ->has(\App\Models\City::factory()->count(4), 'city')
+            ->has(\App\Models\City::factory()->count(1), 'city')
             ->state(new Sequence(
                 fn ($sequence) => ['cityid' => City::all()->random()],
             ))
-            ->count(16)
+            ->count(100*100*100)
             ->create();
     }
 }
